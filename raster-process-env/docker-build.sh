40511a0a3cdd2c0417d1818c0d9de30d91b75889
André Carvalho <afacarvalho@yahoo.com.br>
2020-07-03 15:35:59 -0300

    Improve and clean the docker build defines

---
+++ raster-process-env/docker-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VERSION=$(cat ../raster-process/COMPONENT_VERSION | grep -oP '(?<="version": ")[^"]*')
+export VERSION
+
+# build all images
+docker build -t terrabrasilis/raster-process:v$VERSION --build-arg VERSION=$VERSION -f Dockerfile ../
+
+# send to dockerhub
+echo "The building was finished! Do you want sending this new image to Docker HUB? Type yes to continue." ; read SEND_TO_HUB
+if [[ ! "$SEND_TO_HUB" = "yes" ]]; then
+    echo "Ok, not send the image."
+else
+    echo "Nice, sending the image!"
+    docker push terrabrasilis/raster-process:v$VERSION
+fi
