e841e582fb40a9480a887980ae2e4c12b2cb4da5
André Carvalho <afacarvalho@yahoo.com.br>
2022-02-03 10:02:58 -0300

    Remove unused code

--- cerrado-deter/src/exec.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-from exec_script_service import ExecScriptService
-
-"""
-Running daily with cron job to copy data and process intersections.
-"""
-
-execScriptService = ExecScriptService()
-execScriptService.execScript()
